26794012a0e6791d3731691a392b31d44f20b3cd
Your Name <you@example.com>
2024-05-21 23:31:58 -0700

    shohidul

--- src/hook/useFech.js
+++ src/hook/useFech.js
@@ -1,27 +1,26 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-const useFetch = (endpoint) => {
-    const [data, setData] = useState([]); // Initialize with an empty array
-    const [loading, setLoading] = useState(false);
+const useFetch = (endpoint)=>{
+    const [data,setData] = useState([])
+    const [loading,setLoading] = useState(false)
 
-    const fetchData = async () => {
+    const fetchData = async()=>{
         try {
-            setLoading(true);
-            const response = await axios.get(endpoint);
-            setData(response.data.results);
-            setLoading(false);
+            setLoading(true)
+            const response = await axios.get(endpoint)
+            setLoading(false)
+            setData(response.data.results)
         } catch (error) {
-            console.log(error);
-            setLoading(false);
-        }
-    };
+            console.log('error',error)
+       }
+    }
 
-    useEffect(() => {
-        fetchData();
-    }, [endpoint]); // Add endpoint to dependencies to refetch on change
+    useEffect(()=>{
+        fetchData()
+    },[endpoint])
 
-    return { data, loading };
-};
+    return { data , loading}
+}
 
-export default useFetch;
+export default useFetch
